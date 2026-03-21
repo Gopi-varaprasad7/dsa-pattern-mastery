055541b7c6fa822293d0262e6ee116ee36bd3c2e
Koduri Gopi Varaprasad <kodurigopi98722@gmail.com>
2026-03-21 07:22:34 +0530

    Day 18: LeetCode 5 Longest Palindromic Substring | Expand Around Center

---
+++ Expand/LongestPalindromicSubstring.java
@@ -0,0 +1,37 @@
+// LeetCode 5
+// Pattern : Expand Around Center
+// Time : O(n^2)
+// Space : O(1)
+// Idea : Expand from each index for both odd and even length palindromes.
+
+class Solution {
+    public String longestPalindrome(String s) {
+
+        int start = 0, end = 0;
+
+        for(int i = 0; i < s.length(); i++){
+
+            int len1 = expand(s, i, i);       // odd
+            int len2 = expand(s, i, i + 1);   // even
+
+            int len = Math.max(len1, len2);
+
+            if(len > end - start){
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+
+        return s.substring(start, end + 1);
+    }
+
+    private int expand(String s, int left, int right){
+
+        while(left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            left--;
+            right++;
+        }
+
+        return right - left - 1;
+    }
+}
