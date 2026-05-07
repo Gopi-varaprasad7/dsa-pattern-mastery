42cc54a3a4aae8c3b41026c282fb35a5c42f45b3
Koduri Gopi Varaprasad <kodurigopi98722@gmail.com>
2026-05-08 05:15:44 +0530

    Leetcode 88 Merge Sorted Array

---
+++ 01-Arrays/MergeSortedArray.java
@@ -0,0 +1,29 @@
+// LeetCode 88
+// Pattern : three Pointers
+// Time : O(n)
+// Space : O(1)
+// Idea :Set i and j to last index of an array and use loop until i and j bexomes zero
+
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+        while(i >= 0 && j >= 0){
+            if(nums1[i] > nums2[j]){
+                nums1[k] = nums1[i];
+                i--;
+            }
+            else{
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+        while (j >= 0) {
+            nums1[k] = nums2[j];
+            j--;
+            k--;
+        }
+    }
+}
