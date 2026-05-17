38aa5a8e7515c5263158ecd7713d30629052e3e7
Koduri Gopi Varaprasad <kodurigopi98722@gmail.com>
2026-05-18 05:29:29 +0530

    Leetcode 141 Linked List Cycle

---
+++ 03-Two-Pointers/Solution.java
@@ -0,0 +1,16 @@
+// LeetCode 141
+// Pattern : Two Pointers
+// Time : O(n)
+// Space : O(1)
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) return true;
+        }
+        return false;
+    }
+}
