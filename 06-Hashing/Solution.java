6fedd5887da58865554c6d8bacb50750ec9aee6c
Koduri Gopi Varaprasad <kodurigopi98722@gmail.com>
2026-05-18 06:01:20 +0530

    Leetcode 202 Happy Number

---
+++ 06-Hashing/Solution.java
@@ -0,0 +1,20 @@
+// LeetCode 202
+// Pattern : HashSet
+// Time : O(n2)
+// Space : O(1)
+class Solution {
+    public boolean isHappy(int n) {
+        HashSet<Integer> set = new HashSet<>();
+        while(n != 1 && !set.contains(n)){
+            set.add(n);
+            int sum = 0;
+            while(n > 0){
+                int digit = n % 10;
+                sum += digit * digit;
+                n /= 10;
+            }
+            n = sum;
+        }
+        return n == 1;
+    }
+}
