38547e342730093245cfc439434394be8b44341a
Koduri Gopi Varaprasad <kodurigopi98722@gmail.com>
2026-05-18 06:05:22 +0530

    Leetcode 876 Middle Of LinkedList

---
+++ 03-Two-Pointers/MiddleOfLinkedList.java
@@ -0,0 +1,15 @@
+// LeetCode 876
+// Pattern : Two Pointers
+// Time : O(n)
+// Space : O(1)
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
