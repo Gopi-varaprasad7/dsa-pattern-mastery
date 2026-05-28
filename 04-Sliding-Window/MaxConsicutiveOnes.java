9731e2f25d148c7e5df659d33aca65c98e139e94
Koduri Gopi Varaprasad <kodurigopi98722@gmail.com>
2026-05-28 05:58:59 +0530

    Leetcode 485 Max Consicutive Ones

---
+++ 04-Sliding-Window/MaxConsicutiveOnes.java
@@ -0,0 +1,19 @@
+// LeetCode 485
+// Pattern : Sliding window
+// Time : O(n)
+// Space : O(1)
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int maxCount = 0;
+        int currentCount = 0;
+        for (int num : nums) {
+            if (num == 1) {
+                currentCount++;
+                maxCount = Math.max(maxCount, currentCount);
+            } else {
+                currentCount = 0;
+            }
+        }
+        return maxCount;
+    }
+}
